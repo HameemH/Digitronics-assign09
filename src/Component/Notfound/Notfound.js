b7a18ed2b0f8adde6a0e7f35827ef9769052e337
HameemH <hameemhossain2@gmail.com>
2022-04-03 22:11:26 +0600

    component created and route set

---
+++ src/Component/Notfound/Notfound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Notfound = () => {
+    return (
+        <div>
+            <h1>THE PAGE YOU ARE TRYING TO FIND IS NOT AVAILABLE</h1>
+            <h1>404 NOT FOUND</h1>
+        </div>
+    );
+};
+
+export default Notfound;
