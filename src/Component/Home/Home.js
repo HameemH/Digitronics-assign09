3bd727efcc071674c60242d3e2feceb775696bd7
HameemH <hameemhossain2@gmail.com>
2022-04-03 23:07:55 +0600

    home component done and style added

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-            <h1>This is home</h1>
+        <div className='container'>
+            <div className="container row mt-5 ">
+              <div className="text-section col-6 ">
+                    <h1>Your Next </h1>
+                    <h1>Productivity Beast</h1>
+                    <p>In the new world of web3 and crypto and new other new technology,you really need to keep up with that race.And for that we have the best device for you.</p>
+              </div>
+              <div className="img-section col-6 ">
+                  <img src="https://lappyvalley.com/wp-content/uploads/2021/08/61OvV27-44L._AC_SL1500_.jpg" alt="" />
+              </div>
+            </div>
+            <div className="review-container mt-5">
+                <h1>Reviews</h1>
+            </div>
+            
         </div>
     );
 };
---
+++ src/Hook/fakedata.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const fakedata = () => {
+    const reviews =[
+        // {id:1, name:"Sogir", ratngs:4.4, review:}
+
+    ]
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default fakedata;
