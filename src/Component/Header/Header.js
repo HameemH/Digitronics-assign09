0ce0c2d1841534dd4248f4568c1bfdf6eff2f3b0
HameemH <hameemhossain2@gmail.com>
2022-04-03 22:31:34 +0600

    header section style added

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            <Link to='/home'>Home</Link>
-            <Link to='/reviews'>Reviews</Link>
-            <Link to='/dashboard'>Dashboard</Link>
-            <Link to='/blogs'>Blogs</Link>
-            <Link to='/about'>About</Link>
+        <div className='bg-success p-3 '>
+           <div className='container link-container'>
+            <Link className='text-decoration-none p-1 mx-3 text-white links' to='/home'>Home</Link>
+            <Link className='text-decoration-none p-1 mx-3 text-white links' to='/reviews'>Reviews</Link>
+            <Link className='text-decoration-none p-1 mx-3 text-white links' to='/dashboard'>Dashboard</Link>
+            <Link className='text-decoration-none p-1 mx-3 text-white links' to='/blogs'>Blogs</Link>
+            <Link className='text-decoration-none p-1 mx-3 text-white links' to='/about'>About</Link>
+           </div>
+            
         </div>
     );
 };
